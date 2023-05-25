e7333c3f69c84fa4cf23dc3195f28c03e9877f77
PiyushSaklani <mailforgithubonly@gmail.com>
2023-05-26 00:59:36 +0530

    Social link added and elements are moving with mouse

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
-import MAIN_PAGE from './pages/main_page/main_page';
+import "./App.css";
+import MAIN_PAGE from "./pages/main_page/main_page";
 
 function App() {
   return (
     <div className="App">
-      <MAIN_PAGE />
+      <div className="Pages">
+        <MAIN_PAGE />
+      </div>
+      <div className="SocialMedia">
+        <a href="mailto:saklani302@gmail.com">
+          <div className="Mail" id="SM"></div>
+        </a>
+
+        <a href="https://github.com/PiyushSaklani" target="_blank" rel="noopener noreferrer">
+          <div className="GitHub" id="SM"></div>
+        </a>
+
+        <a href="https://www.linkedin.com/in/piyushsaklani/" target="_blank" rel="noopener noreferrer">
+          <div className="Linkedin" id="SM"></div>
+        </a>
+
+        <div className="SocialMedia-line"></div>
+      </div>
     </div>
   );
 }
--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,25 +1,57 @@
 import React, { useState } from "react";
 import "../landing/landing.css";
-import "../landing/name_animation.css"
+import "../landing/name_animation.css";
 
 function Landing() {
   const [isHovered, setIsHovered] = useState(false);
+  const [location, setLocation] = useState({
+    el1:{t:25,l:85},
+    el2:{t:88,l:90},
+    el3:{t:25,l:3},
+    el4:{t:55,l:6},
+    el5:{t:18,l:51},
+  })
 
-  // const elements = [];
+  const handleMouseMove = (event) => {
+    const { clientX, clientY } = event;
 
-  // for (let i = 0; i < 35; i++) {
-  //   elements.push(<div key={i} className={`re-${i+1}`} id="round-element"></div>);
-  // }
+    setLocation((prevState) => ({
+      el1: {
+        t: 25 + clientX / 1000,
+        l: 85 + clientY / 1000,
+      },
+      el2: {
+        t: 85 + clientX / 1000,
+        l: 86 + clientY / 1000,
+      },
+      el3: {
+        t: 25 + clientX / 1000,
+        l: 3 + clientY / 1000,
+      },
+      el4: {
+        t: 55 + clientY / 1000,
+        l: 6 + clientX / 1000,
+      },
+      el5: {
+        t: 18 + clientY / 1000,
+        l: 51 + clientX / 1000,
+      },
+    }));
+  };
 
   return (
-    <div
-      className={`landing-main-div ${isHovered ? "name-hovered" : ""}`}
-      // onMouseEnter={() => setIsHovered(true)}
-      // onMouseLeave={() => setIsHovered(false)}
+    <div className={`landing-main-div ${isHovered ? "name-hovered" : ""}`}
+    onMouseMove={handleMouseMove}
     >
       <div className="landing-text">
         <div className="landing-greeting">Hello I'm</div>
-        <div className="landing-name" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>Piyush Saklani</div>
+        <div
+          className="landing-name"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          Piyush Saklani
+        </div>
         <div className="landing-intro">
           Motivated individual with a strong interest in web and app
           development. Eager to learn and grow in the field while contributing
@@ -27,14 +59,14 @@ function Landing() {
         </div>
       </div>
       <div className="landing-pic">
-        <div className="landing-svg-element"></div>
+        {/* <div className="landing-svg-element"></div> */}
         <div className="landing-svg-me"></div>
       </div>
-      <div className="landing-element-2" id="element"></div>
-      <div className="landing-element-3" id="element"></div>
-      <div className="landing-element-1" id="element"></div>
-      <div className="landing-element-4" id="element"></div>
-      <div className="landing-element-5" id="element"></div>
+      <div className="landing-element-2" id="element" style={{left:`${location.el2.l}%`, top:`${location.el2.t}%`}}></div>
+      <div className="landing-element-3" id="element" style={{left:`${location.el3.l}%`, top:`${location.el3.t}%`}}></div>
+      <div className="landing-element-1" id="element" style={{left:`${location.el1.l}%`, top:`${location.el1.t}%`}}></div>
+      <div className="landing-element-4" id="element" style={{left:`${location.el4.l}%`, top:`${location.el4.t}%`}}></div>
+      <div className="landing-element-5" id="element" style={{left:`${location.el5.l}%`, top:`${location.el5.t}%`}}></div>
       <div className="landing-element-6" id="element"></div>
       <div className="landing-element-7" id="element"></div>
     </div>
@@ -42,5 +74,3 @@ function Landing() {
 }
 
 export default Landing;
-
-
