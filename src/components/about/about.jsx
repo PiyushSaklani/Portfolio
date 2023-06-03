8123c43d04c8b15b39672337f69817f3b0fc295b
PiyushSaklani <mailforgithubonly@gmail.com>
2023-06-03 16:12:38 +0530

    about and contact page is completed and they are responsive also

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -6,18 +6,20 @@ function ABOUT() {
     <div className="about-main-div" id="about-section">
       <div className="about-main-div-1">
         <div className="about-me">
-        <div className="title-1">ABOUT</div>
-        <br />
+          <div className="title-1">ABOUT</div>
+          <br />
           <div className="about-me-emj">: )</div>
           <br />
-          I am currently pursuing a B.Tech in Computer Science with a specialization in Data Science at NIIT University in Neemrana, Rajasthan.
+          I am currently pursuing a B.Tech in Computer Science with a
+          specialization in Data Science at NIIT University in Neemrana,
+          Rajasthan.
           <br />
           <br />
           In terms of programming languages, I am skilled in Python, Java, SQL,
           JavaScript, TypeScript, HTML/CSS, and R. I have worked with frameworks
           such as React, Node.js, Vue.js, Flutter, FastAPI, and Express.js. I am
           familiar with Git, Docker, Google Cloud Platform, and IDEs such as VS
-          Code, PyCharm, IntelliJ, and Eclipse. 
+          Code, PyCharm, IntelliJ, and Eclipse.
         </div>
       </div>
       <div className="about-main-div-2">
